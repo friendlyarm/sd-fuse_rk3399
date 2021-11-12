3005b25b030b6c1ecd766183732f3874295bc2d9
Lawrence-Tang <tangzongsheng@gmail.com>
2021-11-12 10:09:23 +0800

    update test-big-rootfs-img.sh

--- test/test-big-rootfs-img.sh
+++ test/test-big-rootfs-img.sh
@@ -6,7 +6,7 @@ HTTP_SERVER=112.124.9.243
 # hack for me
 PCNAME=`hostname`
 if [ x"${PCNAME}" = x"tzs-i7pc" ]; then
-       HTTP_SERVER=192.168.1.9
+       HTTP_SERVER=192.168.1.65
 fi
 
 # clean
@@ -16,9 +16,9 @@ sudo rm -rf tmp/*
 cd tmp
 git clone ../../.git sd-fuse_rk3399
 cd sd-fuse_rk3399
-wget http://${HTTP_SERVER}/dvdfiles/rk3399/images-for-eflasher/friendlycore-arm64-images.tgz
+wget http://${HTTP_SERVER}/dvdfiles/RK3399/images-for-eflasher/friendlycore-arm64-images.tgz
 tar xzf friendlycore-arm64-images.tgz
-wget http://${HTTP_SERVER}/dvdfiles/rk3399/images-for-eflasher/emmc-flasher-images.tgz
+wget http://${HTTP_SERVER}/dvdfiles/RK3399/images-for-eflasher/emmc-flasher-images.tgz
 tar xzf emmc-flasher-images.tgz
 
 # make big file
