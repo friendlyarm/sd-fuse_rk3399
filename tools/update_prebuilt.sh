3f03952f406bc2e0118932b4595545c9a0000908
Lawrence-Tang <tangzongsheng@gmail.com>
2023-05-15 16:21:18 +0800

    adjust userdata to 200MB, to avoid potential resize2fs issue.

--- tools/update_prebuilt.sh
+++ tools/update_prebuilt.sh
@@ -12,7 +12,7 @@ cp -f $2/misc.img $1/
 
 RET=0
 if [ ! -f $1/userdata.img ]; then
-	USERDATA_SIZE=104857600
+	USERDATA_SIZE=209715200
 	echo "Generating empty userdata.img (size:${USERDATA_SIZE})"
 	TMPDIR=`mktemp -d`
 	${PWD}/tools/make_ext4fs -s -l ${USERDATA_SIZE} -a root -L userdata $1/userdata.img ${TMPDIR}
