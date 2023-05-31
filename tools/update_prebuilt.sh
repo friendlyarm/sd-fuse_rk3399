4097579e352d5bb849c22d59b69ff60ea8881de0
Lawrence-Tang <tangzongsheng@gmail.com>
2023-05-31 14:38:24 +0800

    Revert "pre-prepared userdata.img"
    This reverts commit 531ef6e6ecea36dc9dc14f1e871aa7ca2e5eaafa.

--- tools/update_prebuilt.sh
+++ tools/update_prebuilt.sh
@@ -9,4 +9,15 @@ set -eu
 cp -f $2/boot.img $1/
 cp -f $2/idbloader.img $1/
 cp -f $2/misc.img $1/
-[ ! -f $1/userdata.img ] && cp -f $2/userdata.img $1/
+
+RET=0
+if [ ! -f $1/userdata.img ]; then
+	USERDATA_SIZE=209715200
+	echo "Generating empty userdata.img (size:${USERDATA_SIZE})"
+	TMPDIR=`mktemp -d`
+	${PWD}/tools/make_ext4fs -s -l ${USERDATA_SIZE} -a root -L userdata $1/userdata.img ${TMPDIR}
+	RET=$?
+	rm -rf ${TMPDIR}
+fi
+
+exit $RET
