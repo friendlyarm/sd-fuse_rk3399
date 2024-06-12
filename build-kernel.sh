077fdb6ba8ed6545fe4276faf5fb14306f3eb990
Lawrence-Tang <tangzongsheng@gmail.com>
2024-06-12 19:20:23 +0800

    build-kernel.sh: update paths and cleanup process

--- build-kernel.sh
+++ build-kernel.sh
@@ -61,14 +61,14 @@ if [ ! -d $OUT ]; then
 	exit 1
 fi
 KMODULES_OUTDIR="${OUT}/output_${SOC}_kmodules"
-true ${kernel_src:=${OUT}/kernel-${SOC}}
-true ${KERNEL_SRC:=${kernel_src}}
+true ${kernel_src:=out/kernel-${SOC}}
+true ${KERNEL_SRC:=$(readlink -f ${kernel_src})}
 
 function usage() {
        echo "Usage: $0 <friendlycore-arm64|friendlydesktop-arm64|buildroot|lubuntu|android7|android8|eflasher>"
        echo "# example:"
        echo "# clone kernel source from github:"
-       echo "    git clone ${KERNEL_REPO} --depth 1 -b ${KERNEL_BRANCH} ${KERNEL_SRC}"
+       echo "    git clone ${KERNEL_REPO} --depth 1 -b ${KERNEL_BRANCH} ${kernel_src}"
        echo "# or clone your local repo:"
        echo "    git clone git@192.168.1.2:/path/to/linux.git --depth 1 -b ${KERNEL_BRANCH} ${KERNEL_SRC}"
        echo "# then"
@@ -141,6 +141,7 @@ if [ ! -d ${KERNEL_SRC} ]; then
 	git clone ${KERNEL_REPO} --depth 1 -b ${KERNEL_BRANCH} ${KERNEL_SRC}
 fi
 
+echo "kernel src: ${KERNEL_SRC}"
 if [ -f "${LOGO}" ]; then
 	cp -f ${LOGO} ${KERNEL_SRC}/logo.bmp
 	echo "using ${LOGO} as logo."
@@ -156,7 +157,7 @@ else
 fi
 
 cd ${KERNEL_SRC}
-make distclean
+make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} distclean
 touch .scmversion
 make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} ${KCFG}
 if [ $? -ne 0 ]; then
