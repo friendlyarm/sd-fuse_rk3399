5fb16d9b6bfaccdc1da251d035d4198963a1f62b
Lawrence-Tang <tangzongsheng@gmail.com>
2024-06-14 15:28:27 +0800

    update build-uboot.sh for Python3 compatibility

--- build-uboot.sh
+++ build-uboot.sh
@@ -74,7 +74,7 @@ if ! [ -x "$(command -v python)" ]; then
     (cd /usr/bin/ && sudo ln -s python2 python)
 fi
 # get include path for this python version
-INCLUDE_PY=$(python -c "from distutils import sysconfig as s; print s.get_config_vars()['INCLUDEPY']")
+INCLUDE_PY=$(python -c "import sysconfig as s; print(s.get_config_vars()['INCLUDEPY'])")
 if [ ! -f "${INCLUDE_PY}/Python.h" ]; then
     sudo apt install python2-dev
 fi
